03791018e821ef345b24babfb598bdf46022c5fc
Nadiafa <nadiafa581@gmail.com>
2019-03-11 16:59:23 +0000

    Done.

---
+++ js/index.js
@@ -0,0 +1,22 @@
+Examples to modify the DOM:
+    Create element:
+    `let element = document.createElement('div');`
+Set properties on it:
+    `element.innerHTML = 'Hello, DOM!';`
+`element.style.backgroundColor = '#f9f9f9';`
+Append elements into the DOM:
+    `document.body.appendChild(element);`
+``
+`js
+    let ul = document.createElement('ul'); 
+    for (let i = 0; i < 3; i++) {
+      let li = document.createElement('li');
+      li.innerHTML = (i + 1).toString();
+      ul.appendChild(li);
+    }
+    element.appendChild(ul);
+  `
+``
+Remove elements from the DOM:
+    `ul.remove();`
+`ul.removeChild(ul.querySelector('li:nth-child(2)'));`
